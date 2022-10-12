0711f365ba59a3a50bce6fcd2a1c66ce2b2f2196
zyh-1234 <3550590260@qq.com>
2022-10-12 16:09:32 +0800

    init

--- core/service/request/config.js
+++ core/service/request/config.js
@@ -1,12 +1,12 @@
 // export const API_BASE_URL = process.env
 
-export const API_LOCAL_URL = 'http://localhost:3000'
+// export const API_LOCAL_URL = 'http://localhost:3000'
 
 //localhost test server
 // export const API_LOCAL_URL = 'http://localhost:3001'
 
 // test server
-// export const API_LOCAL_URL = 'http://47.107.93.191:3001'
+export const API_LOCAL_URL = 'http://47.107.93.191:3001'
 
 // dependencies server
 export const API_BASE_URL = 'https://apiwww.fallon-fj.com'
